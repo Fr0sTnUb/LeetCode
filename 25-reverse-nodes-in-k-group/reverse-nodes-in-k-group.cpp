b52870a55d939679d36798c9d047433c6c303be6
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-13 13:47:44 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (38.43%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -10,30 +10,52 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* &head, int k, ListNode* prev){
+        ListNode* previous = prev;
+        ListNode* current = head;
+        ListNode* nextNode = nullptr;
+
+        while(k--){
+            nextNode = current->next;
+            current->next = previous;
+            previous = current;
+            current = nextNode;
+        }
+
+        return previous;
+
+    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if(head == NULL || head->next == NULL) return head;
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* curr = dummy;
-        ListNode* prev = dummy;
-        ListNode* nex = dummy;
-        int count = 0;
-        while(curr->next != NULL){
-            curr = curr->next;
-            count++;
-        }
-        while(count >= k){
-            curr = prev->next;
-            nex = curr->next;
-            for(int i = 1; i < k; i++){
-                curr->next = nex->next;
-                nex->next = prev->next;
-                prev->next = nex;
-                nex = curr->next;
+        if(head == nullptr || k==1) return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* prev = &dummy;
+        ListNode* current = head;
+
+
+
+        while(true){
+            ListNode* temp = current;
+            int count = 0;
+
+            while(temp && count<k){
+                temp = temp->next;
+                count++;
             }
-            prev = curr;
-            count -= k;
+
+            if(count < k) break;
+
+            ListNode* nextGroup = temp;
+            ListNode* newhead = reverse(current, k, nextGroup);
+
+            prev->next = newhead;
+            prev = current;
+            current = nextGroup;
         }
-        return dummy->next;
+
+        return dummy.next;
+
     }
 };
