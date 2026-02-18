b40f48b9a71fa50f2f58910d1a3406a88a3d74cd
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-02-18 14:42:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (91.71%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int> st;
+        int n = nums2.size();
+        vector<int> ans(n, -1);
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && nums2[st.top()] < nums2[i]){
+                ans[st.top()] = nums2[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        vector<int> r;
+        for(int i = 0; i < nums1.size(); i++){
+            int idx = find(nums2.begin(), nums2.end(), nums1[i]) - nums2.begin();
+            r.push_back(ans[idx]);
+        }
+        return r;
+    }
+};
