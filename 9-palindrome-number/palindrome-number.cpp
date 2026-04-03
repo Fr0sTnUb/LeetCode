13f978f67b44fcee2c0b29b10f3421e4b53b0294
Nitesh Sha <85791340+Fr0sTnUb@users.noreply.github.com>
2026-04-03 21:41:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (64.95%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int o = x;
+        long long r = 0;
+        while(x>0){
+            int digit = x % 10;
+            r = r*10 + digit;
+            x/=10;
+        }
+        return o == r;
+    }
+};
